d24b9881c2531c7c79d935e9daedcc2a1f23fc9c
Swapnil <swapnilpisal24@gmail.com>
2023-12-30 10:21:52 +0530

    Iutility Interface implimented

---
+++ Repositorypattern.Repositories/Implimentations/UtilityRepo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Repositorypattern.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorypattern.Repositories.Implimentations
+{
+    public class UtilityRepo : IUtilityRepo
+    {
+        public Task DeleteFile(string ContainerName, string dbPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> EditImage(string ContainerName, IFormFile formFile, string dbPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveImage(string ContainerName, IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
