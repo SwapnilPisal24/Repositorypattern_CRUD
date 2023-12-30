bbabea9f8c7edb76d319ee3aad18051113e552d2
SWAPNIL <SWAPNIL@DESKTOP-905RBDS>
2023-12-30 09:53:16 +0530

    IutilityInterface added & CountryImage folder added in root

---
+++ Repositorypattern.Repositories/Interfaces/IUtilityRepo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorypattern.Repositories.Interfaces
+{
+    public interface IUtilityRepo
+    {
+        Task<string> SaveImage(string ContainerName, IFormFile formFile);   
+        Task<string> EditImage(string ContainerName, IFormFile formFile, string dbPath);
+        Task DeleteFile(string ContainerName, string dbPath);
+    }
+}
